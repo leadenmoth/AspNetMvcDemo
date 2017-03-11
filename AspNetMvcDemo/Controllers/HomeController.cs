b6388ce1d959fb0dba4279e4670d246fe6072d89
Artem Astashenkov <astashenkovaa@gmail.com>
2017-03-11 22:42:32 +0200

    Front End Task 1 (Initial commit)

---
+++ AspNetMvcDemo/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using AspNetMvcDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvcDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ///Hardcoding some demo values for now
+            List<Product> products = new List<Product>
+            {
+                new Product()
+                {
+                    ID = 1,
+                    Category = "Vegetables",
+                    Name = "Potatoes",
+                    Price = 1.99M,
+                    Description = "Price per kg",
+                    Modified = DateTime.Now
+                },
+                new Product()
+                {
+                    ID = 2,
+                    Category = "Vegetables",
+                    Name = "Cucumbers",
+                    Price = 2.99M,
+                    Description = "Price per kg",
+                    Modified = DateTime.Now
+                },
+                new Product()
+                {
+                    ID = 3,
+                    Category = "Vegetables",
+                    Name = "Carrots",
+                    Price = 1.99M,
+                    Description = "Price per kg",
+                    Modified = DateTime.Now
+                },
+                new Product()
+                {
+                    ID = 4,
+                    Category = "Fruit",
+                    Name = "Apples",
+                    Price = 2.50M,
+                    Description = "Price per kg",
+                    Modified = DateTime.Now
+                },
+                new Product()
+                {
+                    ID = 5,
+                    Category = "Fruit",
+                    Name = "Bananas",
+                    Price = 1.99M,
+                    Description = "Price per kg",
+                    Modified = DateTime.Now
+                }
+            };
+            return View(products);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Placeholder 'About' page";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Placeholder 'Contact' page";
+
+            return View();
+        }
+    }
+}
---
+++ AspNetMvcDemo/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetMvcDemo.Models
+{
+    /// <summary>
+    /// Using model for back end task 2 as a placeholder for now
+    /// </summary>
+    public class Product
+    {
+        [Display(Name = "Product Number")]
+        public int ID { get; set; }
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public DateTime Modified { get; set; }
+    }
+}
