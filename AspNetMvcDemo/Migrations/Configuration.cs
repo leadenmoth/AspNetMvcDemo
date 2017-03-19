fe42197a9bc9324351fbb71173904b09996df9e2
Artem Astashenkov <astashenkovaa@gmail.com>
2017-03-19 20:30:48 +0200

    Bonus Task (seed data for Azure deployment)

--- AspNetMvcDemo/Migrations/Configuration.cs
+++ AspNetMvcDemo/Migrations/Configuration.cs
@@ -1,6 +1,8 @@
 namespace AspNetMvcDemo.Migrations
 {
+    using AspNetMvcDemo.Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -15,18 +17,51 @@ namespace AspNetMvcDemo.Migrations
 
         protected override void Seed(AspNetMvcDemo.Models.ProductDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            List<Product> products = new List<Product>
+            {
+                new Product()
+                {
+                    Category = "Vegetables",
+                    Name = "Potatoes",
+                    Price = 1.99M,
+                    Description = "Price per kg",
+                    Modified = DateTime.Now
+                },
+                new Product()
+                {
+                    Category = "Vegetables",
+                    Name = "Cucumbers",
+                    Price = 2.99M,
+                    Description = "Price per kg",
+                    Modified = DateTime.Now
+                },
+                new Product()
+                {
+                    Category = "Vegetables",
+                    Name = "Carrots",
+                    Price = 1.99M,
+                    Description = "Price per kg",
+                    Modified = DateTime.Now
+                },
+                new Product()
+                {
+                    Category = "Fruit",
+                    Name = "Apples",
+                    Price = 2.50M,
+                    Description = "Price per kg",
+                    Modified = DateTime.Now
+                },
+                new Product()
+                {
+                    Category = "Fruit",
+                    Name = "Bananas",
+                    Price = 1.99M,
+                    Description = "Price per kg",
+                    Modified = DateTime.Now
+                }
+            };
+            products.ForEach(s => context.Products.AddOrUpdate(p => p.Name, s));
+            context.SaveChanges();
         }
     }
 }
